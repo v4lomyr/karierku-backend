d2a00baa831039078d476371d925fb78ca835afe
v4lomyr <bintangdwitama160501@gmail.com>
2022-08-29 20:52:50 +0700

    🐱‍💻fix database connection

--- model/Remote.go
+++ model/Remote.go
@@ -2,12 +2,16 @@ package model
 
 import (
 	"database/sql"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
 func ConnectDB() (*sql.DB, error) {
-	connStr := "postgres://bintang:bintang@localhost/karierku?sslmode=disable"
+	connStr := os.Getenv("DATABASE_URL")
+	if connStr == "" {
+		connStr = "postgres://bintang:bintang@localhost/karierku?sslmode=disable"
+	}
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		return nil, err
